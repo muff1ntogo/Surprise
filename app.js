36488e6b59673ab323f9b82e558250c63170490b
muff1ntogo <49677630+BappoNUKED@users.noreply.github.com>
2024-06-02 18:51:41 +0100

    Added Secret Doc link code

--- app.js
+++ app.js
@@ -2,13 +2,13 @@ const days = document.getElementById('days');
 const hours = document.getElementById('hours');
 const minutes = document.getElementById('minutes');
 const seconds = document.getElementById('seconds');
+const secretLink = document.getElementById('secret-link'); // Get the secret link element
 
 const currentYear = new Date().getFullYear();
 
 const releasedate = new Date(`September 2 2024 10:00:00`);
 
-
-//Update Countdowntime
+// Update Countdown time
 function updateCountdown() {
     const currentTime = new Date();
     const diff = releasedate - currentTime;
@@ -18,14 +18,22 @@ function updateCountdown() {
     const m = Math.floor(diff / 1000 / 60) % 60;
     const s = Math.floor(diff / 1000) % 60;
 
-    days.innerHTML= d;
-    hours.innerHTML= h < 10 ? '0' + h : h;
-    minutes.innerHTML= m < 10 ? '0' + m : m;
-    seconds.innerHTML= s < 10 ? '0' + s : s;
+    if (diff > 0) {
+        days.innerHTML = d;
+        hours.innerHTML = h < 10 ? '0' + h : h;
+        minutes.innerHTML = m < 10 ? '0' + m : m;
+        seconds.innerHTML = s < 10 ? '0' + s : s;
+    } else {
+        days.innerHTML = '0';
+        hours.innerHTML = '00';
+        minutes.innerHTML = '00';
+        seconds.innerHTML = '00';
+        secretLink.style.display = 'block'; // Show the secret link when the countdown ends
+    }
 }
 
+setInterval(updateCountdown, 1000);
 
-setInterval(updateCountdown, 1000)
 
 
 
